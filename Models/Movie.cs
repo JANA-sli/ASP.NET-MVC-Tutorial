873dd05b613969d18f16e272d4715f4b86b887bb
Sunny Li <sli@janatechnology.com>
2020-01-02 15:55:36 -0500

    add search and additional model validations

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,16 +1,25 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MvcMovie.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
 
+        [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
         public string Genre { get; set; }
+
+        [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
     }
 }
